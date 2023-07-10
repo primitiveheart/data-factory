9ac9dd823984cdc42317f88771f0203c0318b785
周官宝 <zhouguanbao@192.168.1.2>
2023-07-10 21:46:36 +0800

    初始化

---
+++ src/main/java/com/sky/data/factory/DataFactoryApplication.java
@@ -0,0 +1,13 @@
+package com.sky.data.factory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataFactoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataFactoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sky/data/factory/DataFactoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sky.data.factory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataFactoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
